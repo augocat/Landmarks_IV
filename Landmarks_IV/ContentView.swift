f973de4ff8e2ff81b0246752c511b2e082e97b4d
narb <narborough.grahame@icloud.com>
2024-08-01 18:44:07 +1000

    Initial Commit

---
+++ Landmarks_IV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks_IV
+//
+//  Created by Grahame Narborough on 1/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks_IV/Landmarks_IVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_IVApp.swift
+//  Landmarks_IV
+//
+//  Created by Grahame Narborough on 1/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_IVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
