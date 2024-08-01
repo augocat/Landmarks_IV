ce7663695af9180096f0fdc1ccd813e1c0568be8
narb <narborough.grahame@icloud.com>
2024-08-02 08:55:54 +1000

    Text only changed

--- Landmarks_IV/Landmarks_IVApp.swift
+++ Landmarks_IV/Landmarks_IVApp.swift
@@ -1,17 +1,10 @@
-//
-//  Landmarks_IVApp.swift
-//  Landmarks_IV
-//
-//  Created by Grahame Narborough on 1/8/2024.
-//
-
 import SwiftUI
 
 @main
 struct Landmarks_IVApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
