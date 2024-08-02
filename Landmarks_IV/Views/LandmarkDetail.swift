8cb289fdce03052f129aec16e563cd6775d8c5f1
narb <narborough.grahame@icloud.com>
2024-08-02 21:21:18 +1000

    Add Scrolling to Detail view

--- Landmarks_IV/Views/LandmarkDetail.swift
+++ Landmarks_IV/Views/LandmarkDetail.swift
@@ -4,7 +4,7 @@ struct LandmarkDetail: View {
   var landmark: Landmark
 
   var body: some View {
-    VStack {
+    ScrollView {
       MapView(coordinate: landmark.locationCoordinate)
         .frame(height: 300.0)
       CircleImage(image: landmark.image)
@@ -26,6 +26,8 @@ struct LandmarkDetail: View {
       .padding()
       Spacer()
     }
+    .navigationTitle(landmark.name)
+    .navigationBarTitleDisplayMode(.inline)
   }
 }
 
