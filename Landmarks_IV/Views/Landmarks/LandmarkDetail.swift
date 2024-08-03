f7843d01f3365c3fc29fcd2c063d590f8534a3ea
narb <narborough.grahame@icloud.com>
2024-08-03 14:01:01 +1000

    Add Favirites button for each Landmark at List view

--- Landmarks_IV/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks_IV/Views/Landmarks/LandmarkDetail.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+  @Environment(ModelData.self) var modelData
   var landmark: Landmark
+  
+  var landmarkIndex: Int {
+    modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+  }
 
   var body: some View {
+    @Bindable var modelData = modelData
     ScrollView {
       MapView(coordinate: landmark.locationCoordinate)
         .frame(height: 300.0)
@@ -11,8 +17,11 @@ struct LandmarkDetail: View {
         .offset(y: -130)
         .padding(.bottom, -130)
       VStack(alignment: .leading) {
-        Text(landmark.name)
-          .font(.title)
+        HStack {
+          Text(landmark.name)
+            .font(.title)
+          FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+        }
         HStack {
           Text(landmark.park)
           Spacer()
@@ -32,5 +41,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-  LandmarkDetail(landmark: ModelData().landmarks[0])
+  let modelData = ModelData()
+  return LandmarkDetail(landmark: modelData.landmarks[0])
+    .environment(modelData)
 }
